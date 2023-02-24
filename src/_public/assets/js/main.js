d9ead3755364f5ce7ef6ee6d22dd6a06d5f74121
Hoa Ngo <dachoa.agl@gmail.com>
2023-02-24 17:27:26 +0700

    edit

--- src/_public/assets/js/main.js
+++ src/_public/assets/js/main.js
@@ -1,42 +1,27 @@
-let currentSlide = 0, theSlides = document.getElementsByClassName('widget_media_image'), customSliderTimer, next, prev, theWrapper = document.getElementById('my-custom-slider-demo');
-console.log(theSlides);
-function customNavToNextSlide() {
-	next = ( currentSlide < theSlides.length - 1 ) ? currentSlide + 1 : 0;
-	customSetCurrentSlide(next);
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+	showSlides(slideIndex += n);
 }
-function customNavToPrevSlide() {
-	prev = ( currentSlide > 0 ) ? currentSlide - 1 : theSlides.length - 1;
-	customSetCurrentSlide(prev);
+
+// Thumbnail image controls
+function currentSlide(n) {
+	showSlides(slideIndex = n);
 }
-function customSetCurrentSlide(to) {
-	clearInterval(customSliderTimer);
-	for (let i = 0; i < theSlides.length; i ++) {
-		if ( currentSlide == i ) {
-			theSlides[currentSlide].classList.remove('active');
-		}
-		if ( next == i ) {
-			theSlides[to].classList.add('active');
-		}
+
+function showSlides(n) {
+	let i;
+	let slides = document.getElementsByClassName("mySlides");
+	
+	if (n > slides.length) { slideIndex = 1 }
+	if (n < 1) { slideIndex = slides.length }
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
 	}
-	currentSlide = to;
-	customSliderStart();
+	
+	slides[slideIndex - 1].style.display = "block";
+
 }
-function customSliderStart() {
-	customSliderTimer = setInterval(function() {
-		customNavToNextSlide();
-	}, 5000);
-}
-function customSliderInit() {
-	let height = theSlides[0].getElementsByTagName('img')[0].height;
-	theWrapper.style.height = height + 'px';
-	for (let i = 0; i < theSlides.length; i ++) {
-		theSlides[i].style.height = height + 'px';
-	}
-}
-if (typeof theSlides !== 'undefined') {
-	window.addEventListener('resize', customSliderInit);
-	customSliderInit();
-	if ( theSlides.length > 1 ) {
-		customSliderStart();
-	}
-}
+
