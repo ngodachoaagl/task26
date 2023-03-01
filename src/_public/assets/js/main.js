6d6079c84623d4c71c7331d6f9c8e3ac765edff6
Hoa Ngo <dachoa.agl@gmail.com>
2023-03-01 16:32:34 +0700

    edit

--- src/_public/assets/js/main.js
+++ src/_public/assets/js/main.js
@@ -1,19 +1,19 @@
 let slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous controls
+
 function plusSlides(n) {
 	showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
+
 function currentSlide(n) {
 	showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
 	let i;
-	let slides = document.getElementsByClassName("mySlides");
+	let slides = document.getElementsByClassName("c-mySlides");
 	
 	if (n > slides.length) { slideIndex = 1 }
 	if (n < 1) { slideIndex = slides.length }
