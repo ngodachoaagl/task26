2f1eebd535bb6197b569f2905d39d58c4ab84248
Hoa Ngo <dachoa.agl@gmail.com>
2023-03-03 17:24:12 +0700

    edit

--- src/_public/assets/js/main.js
+++ src/_public/assets/js/main.js
@@ -1,27 +1,13 @@
-let slideIndex = 1;
-showSlides(slideIndex);
 
-
-function plusSlides(n) {
-	showSlides(slideIndex += n);
-}
-
-
-function currentSlide(n) {
-	showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-	let i;
-	let slides = document.getElementsByClassName("c-mySlides");
-	
-	if (n > slides.length) { slideIndex = 1 }
-	if (n < 1) { slideIndex = slides.length }
-	for (i = 0; i < slides.length; i++) {
-		slides[i].style.display = "none";
-	}
-	
-	slides[slideIndex - 1].style.display = "block";
-
-}
-
+$(".c-mySlides").slick({
+    dots: false,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    cssEase: "linear",
+	prevArrow: $(".c-prev"),
+	nextArrow: $(".c-next"),
+  });
