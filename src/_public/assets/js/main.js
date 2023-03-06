c9adda8f304f7da647d07da72f9c9453749be60d
Hoa Ngo <dachoa.agl@gmail.com>
2023-03-06 15:29:49 +0700

    edit

--- src/_public/assets/js/main.js
+++ src/_public/assets/js/main.js
@@ -1,13 +1,14 @@
-
-$(".c-mySlides").slick({
+$(document).ready(function () {
+  $(".c-mySlides").slick({
     dots: false,
     infinite: true,
     speed: 1000,
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 5000,
+    autoplaySpeed: 2000,
     cssEase: "linear",
-	prevArrow: $(".c-prev"),
-	nextArrow: $(".c-next"),
+    prevArrow: $(".c-prev"),
+    nextArrow: $(".c-next"),
   });
+});
